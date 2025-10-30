bbe85238c5c84d263297da460a1bb1b54bb07e56
Milton Mamani <miltin.mamani010@gmail.com>
2025-10-30 00:09:53 -0300

    comence con las importaciones del register ya que me estaba saliendo un error de sintaxis

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import { useForm } from "react-hook-form";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Link } from "react-router";
+import "../Styles/register.css";
+import icono from "../img/icono-veterinario.png";
+import Swal from "sweetalert2";
+import { crearUsuario } from "../../helpers/queries";
 
 const Register = () => {
     return (

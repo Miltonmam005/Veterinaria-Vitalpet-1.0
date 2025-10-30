36cfc2b0756b63649d92213d4bad8cb3439da548
Milton Mamani <miltin.mamani010@gmail.com>
2025-10-29 23:40:35 -0300

    agrege el carrusel del inicio que sera una foto

---
+++ src/components/Inicio.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router";
+import React from 'react';
+
+const Inicio = () => {
+    return (
+        <div>
+            
+        </div>
+   );
+};
+
+export default Inicio;

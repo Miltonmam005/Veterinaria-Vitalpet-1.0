621d1c9208dd14a4367e12318ebb1a52641fca2f
Milton Mamani <miltin.mamani010@gmail.com>
2025-10-29 23:42:46 -0300

    cree los planes con algunos botones

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router";
-import React from 'react';
+import React from "react";
 
 const Inicio = () => {
   return (

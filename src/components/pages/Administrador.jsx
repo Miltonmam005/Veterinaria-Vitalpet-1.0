af7718271f1c6e9bfaf45c6a8f471e77f869bbbd
Milton Mamani <miltin.mamani010@gmail.com>
2025-11-01 23:24:05 -0300

    agrege una lista donde tendrra un id nombre tipo precio categotia y opciones

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -2,3 +2,15 @@ import { Button, Table } from "react-bootstrap";
 import ItemProductos from "./componentesAdministrador/ItemProductos.jsx";
 import { Link } from "react-router";
 import { cardsData } from "../data/cardsData.js";
+
+const Administrador = ({
+  productos,
+  setProductos,
+  borrarProducto,
+  destacarProducto,
+}) => {
+  const cargarDatosPrueba = () => {
+    setProductos(cardsData);
+  };
+
+  

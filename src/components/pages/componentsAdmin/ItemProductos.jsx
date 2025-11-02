ba2df0b72ec2c545ef45e6663be9a95cb88435e8
Milton Mamani <miltin.mamani010@gmail.com>
2025-11-01 23:30:52 -0300

    agrege de paso la logica del proyecto con un productO.Title

--- src/components/pages/componentsAdmin/ItemProductos.jsx
+++ src/components/pages/componentsAdmin/ItemProductos.jsx
@@ -1,3 +1,33 @@
 import { Button } from "react-bootstrap";
 import { Link } from "react-router";
 import Swal from "sweetalert2";
+
+const ItemProducto = ({ producto, fila, borrarProducto, destacarProducto }) => {
+  const eliminarProducto = () => {
+    Swal.fire({
+      title: "Eliminar producto",
+      text: "No puedes revertir este paso!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#277a35ff",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, quiero eliminar!",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        if (borrarProducto(producto.id)) {
+          Swal.fire({
+            title: "Producto eliminado!",
+            text: `El producto ${producto.title} fue eliminado correctamente`,
+            icon: "success",
+          });
+        } else {
+          Swal.fire({
+            title: "Ocurrió un error!",
+            text: `El producto ${producto.title} no pudo ser eliminado`,
+            icon: "error",
+          });
+        }
+      }
+    });
+  };

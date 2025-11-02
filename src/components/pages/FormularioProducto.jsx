a5cd7f5f280268c3f0dcc09bb7fc60c1a1d63ba6
Milton Mamani <miltin.mamani010@gmail.com>
2025-11-01 21:32:47 -0300

    comence a trabajar con el form y algunas clases de bootstrap

---
+++ src/components/pages/FormularioProducto.jsx
@@ -0,0 +1,5 @@
+import { useEffect } from "react";
+import { Button, Form, Container, Row, Col, Card } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate, useParams } from "react-router";
+import Swal from "sweetalert2";

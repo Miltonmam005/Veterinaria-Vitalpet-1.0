9f0320d89e49149fc906ae4923a8faf4d420aa87
Milton Mamani <miltin.mamani010@gmail.com>
2025-11-01 22:40:25 -0300

    agrege la imagen del error 404

---
+++ src/components/pages/Error404.jsx
@@ -0,0 +1,32 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router";
+import errorGato from "../../assets/errorgat.404.jpg";
+import "../Styles/Error404.css";
+
+const Error404 = () => {
+  return (
+    <section className="container-fluid bg-light text-dark">
+      <Container className="text-center py-5">
+        <Row>
+          <Col>
+            <img
+              src={errorGato}
+              alt="Página no encontrada"
+              className="img-fluid errorImg mb-4"
+            />
+            <h2>Lo sentimos, página no encontrada</h2>
+            <p>
+              La página a la que intentaste acceder no existe o ha sido
+              eliminada
+            </p>
+            <Link to="/" className="btn btn-outline-success">
+              Volver al Inicio
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Error404;

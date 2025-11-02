6f3c68699ab98938d8c676fedbf4be699e312914
Milton Mamani <miltin.mamani010@gmail.com>
2025-11-01 21:18:15 -0300

    use usernavegate

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import Swal from 'sweetalert2';
+import icono from '..img/icono-veterinaria.png';
+import { Link, useNavigate } from 'react-router';
+import { login } from "../../helpers/queries";
 
-const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
 
-export default Login;

8771f6ab056d9260e245a75c8473945c25af11ef
Milton Mamani <miltin.mamani010@gmail.com>
2025-11-01 23:23:02 -0300

    agrege un link de agregar productos en forma de btn

---
+++ src/components/pages/Administrador.jsx
@@ -0,0 +1,4 @@
+import { Button, Table } from "react-bootstrap";
+import ItemProductos from "./componentesAdministrador/ItemProductos.jsx";
+import { Link } from "react-router";
+import { cardsData } from "../data/cardsData.js";

4c11d4f28829139f373ee54131010097dcbb5d00
Milton Mamani <miltin.mamani010@gmail.com>
2025-11-01 22:29:50 -0300

    inicie con la logica tambien

--- src/components/pages/FormularioPlan.jsx
+++ src/components/pages/FormularioPlan.jsx
@@ -3,3 +3,26 @@ import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
 
+function FormularioPlan() {
+  const { planNombre } = useParams();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmitForm = (data) => {
+    console.log("Formulario enviado:", {
+      ...data,
+      planSeleccionado: planNombre,
+    });
+    Swal.fire({
+      title: `Gracias ${data.nombre}`,
+      text: " el plan ha sido elegido!",
+      icon: "success",
+    }).then(() => {
+      reset();
+    });
+  };

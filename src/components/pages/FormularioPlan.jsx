fef858c8bc25fc0d37eba3b31bf0fbe6a35b6e5a
Milton Mamani <miltin.mamani010@gmail.com>
2025-11-01 22:28:59 -0300

    continue trabajando con use params

--- src/components/pages/FormularioPlan.jsx
+++ src/components/pages/FormularioPlan.jsx
@@ -1,11 +1,5 @@
-import React from 'react';
+import { useParams } from "react-router";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 
-const FormularioPlan = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default FormularioPlan;
